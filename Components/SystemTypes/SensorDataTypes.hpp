e244cd64915509e9a75683642d44a0c0c84db740
shivamdesai04 <101665374+shivamdesai04@users.noreply.github.com>
2024-11-03 17:30:37 -0700

    clang format fix

--- Components/SystemTypes/SensorDataTypes.hpp
+++ Components/SystemTypes/SensorDataTypes.hpp
@@ -3,7 +3,7 @@
  * @file    SensorDataTypes.hpp
  * @author  Shivam Desai
  * @date    Nov 3, 2024
- * @brief	General sensor data structure to pass around in the system or
+ * @brief   General sensor data structure to pass around in the system or
  *log it to flash memory
  ********************************************************************************
  */
